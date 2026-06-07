7021c441c2ad6e41e82afe58020941b409909c63
Imad <sparkdz.services@gmail.com>
2026-06-07 13:45:23 +0100

    Revert next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    runtime: 'edge',
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
